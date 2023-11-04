bafe8587359b0fabe6534cbf55d99938aed2ce9b
Keep-Konect <66421869+konect-V@users.noreply.github.com>
2023-11-04 18:10:54 +0100

    Add pci example

---
+++ examples/pci.c
@@ -0,0 +1,24 @@
+#include "../odi-light/odi-ocf.c"
+
+void odi_examples_pci(void* mcfg_table){
+    /* example of the pci driver implementation */
+    odi_pci_handler_t* odi_pci_handler = odi_drivers_bus_odi_pci_init(acpi_find_table(acpi_rsdp, "MCFG"));
+    odi_pci_device_info_t odi_pci_device_info = (odi_pci_device_info_t){
+        ODI_PCI_SEARCH_NO_PARAMETER, 
+        ODI_PCI_SEARCH_NO_PARAMETER, 
+        ODI_PCI_SEARCH_NO_PARAMETER,
+        ODI_PCI_SEARCH_NO_PARAMETER,
+        ODI_PCI_SEARCH_NO_PARAMETER };
+    
+    size_t odi_pci_device_detected = odi_pci_handler->count_devices(&odi_pci_device_info);
+
+    odi_dep_printf("[module/odi/pci] %d device%s detected\n", odi_pci_device_detected, odi_pci_device_detected > 1 ? "s" : "");
+
+    for(size_t i = 0; i < odi_pci_device_detected; i++){
+        odi_pci_device_id_t device_id = odi_pci_handler->find_device(&odi_pci_device_info, i);
+        odi_pci_device_info_t device_info;
+        assert(!odi_pci_handler->get_info_device(device_id, &device_info));
+        odi_dep_printf("[module/odi/pci/device/%d] Vendor : %d, Device ID : %d, Class ID : %d, Subclass ID : %d, Programming Interface Byte : %d\n", i, device_info.vendor_id, device_info.device_id, device_info.class_id, device_info.sub_class_id, device_info.prog_if);
+    }
+
+}
--- scripts/ocf.sh
+++ scripts/ocf.sh
@@ -15,10 +15,17 @@ if [ ! -f "$config_path" ]; then
   cp $config_default_path $config_path
 fi
 
+echo "#ifndef ODI_OFC" >> "$ocf_path"
+echo "#define ODI_OFC" >> "$ocf_path"
+echo "" >> "$ocf_path"
+
 echo "#include \"$(realpath "$config_path")\"" >> "$ocf_path"
 
 for c_file in $c_files; do
   echo "#include \"$(realpath "$c_file")\"" >> "$ocf_path"
 done
+
+echo "" >> "$ocf_path"
+echo "#endif // ODI_OFC" >> "$ocf_path"
 
 echo "OCF (One C File) created with success : $ocf_path"
